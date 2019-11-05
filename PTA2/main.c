ecc76c1f405911e6c5127eaa9e39483bbb665436
zengkaiwei2018 <1912207369@qq,com>
2019-11-05 16:49:40 +0800

    PTA7-5
      简化的插入排序

--- PTA2/main.c
+++ PTA2/main.c
@@ -1,28 +1,31 @@
-//
-//  main.c
-//  PTA2
-//
-//  Created by s20181105306 on 2019/11/5.
-//  Copyright © 2019 s20181105306. All rights reserved.
-//
-
 #include<stdio.h>
-int main(){
-    int a[10]={0},n,i,j,num,max=0;
+int main()
+{
+    int a[11],i,n,X,k;
     scanf("%d",&n);
-    for(i=0;i<n;i++){
-        scanf("%d",&num);
-        while(num){
-            a[num%10]++;
-            num/=10;
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    scanf("%d",&X);
+    if(X<a[0])
+    {for(k=n-1;k>=0;k--)
+        a[k+1]=a[k];
+        a[0]=X;
+    }
+    else if(X>a[n-1])
+        a[n]=X;
+    
+    else
+    {for(i=0;i<n-1;i++)
+    {
+        if(a[i]<X&&a[i+1]>X)
+        { for(k=n-1;k>i;k--)
+            a[k+1]=a[k];
+            a[i+1]=X;
+            break;
         }
     }
-    for(i=0;i<10;i++)
-        if(max<a[i])
-            max=a[i];
-    printf("%d:",max);
-    for(i=0;i<10;i++)
-        if(max==a[i])
-            printf(" %d",i);
+    }
+    for(i=0;i<=n;i++)
+        printf("%d ",a[i]);
     return 0;
 }
